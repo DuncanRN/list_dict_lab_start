9deeb2a8854f76b64c21758a61e9fdb7ea9ec2d8
Duncan Robertson <knacknud@gmail.com>
2022-08-30 14:56:34 +0100

    first done

--- exercise_c_extensions_uk.py
+++ exercise_c_extensions_uk.py
@@ -16,6 +16,8 @@ united_kingdom = [
   }
 ]
 
+#LIST OF DICTIONARIES
+
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
 # 2. Create a dictionary for Northern Ireland and add it to the `united_kingdom` list (The capital is Belfast, and the population is 1,811,000).
 # 3. Use a loop to print the names of all the countries in the UK.
